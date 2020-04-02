3437bdfa82f553a126732b19efcc651a515688f8
413831 <alepleins3pp@gmail.com>
2020-04-02 10:59:03 -0300

    Update

---
+++ TP_4_ABM_LinkedList/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../inc/Controller.h"
+#include "../inc/LinkedList.h"
+#include "../inc/Employee.h"
+#include "../inc/utn.h"
+#include "../inc/parser.h"
+
+//Archivo de prueba data.csv
+int main()
+{
+    controller_init();
+    return 0;
+}
